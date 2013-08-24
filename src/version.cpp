92f4beaf3c1d0e3b8eb9f377d8aabaf98d9b3fea
Mikael Hirki <mikaelh@mbnet.fi>
2013-08-24 21:24:29 +0300

    Bump version to -hp10 in anticipation of the upcoming release.

--- src/version.cpp
+++ src/version.cpp
@@ -12,7 +12,7 @@
 const std::string CLIENT_NAME("Satoshi");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-beta-hp9"
+#define CLIENT_VERSION_SUFFIX   "-beta-hp10"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
